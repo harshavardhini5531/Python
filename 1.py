ef69dc957d3f4baf822ca42e8bb0e69a105b9d23
harshavardhini5531 <harshavardhini.j@adityauniversity.in>
2025-09-22 12:01:50 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+plt.plot(x, y)
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+plt.title('Simple Line Graph')
+plt.show()
